7d29489607e2fee01f56aed2554d1e5f669f77ab
alex <alexalarcon200@gmail.com>
2025-02-11 13:58:12 -0500

    added homepage image

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,10 @@
+
 const Home = () => {
     return (
-    <div>
+    <div className="home">
         <h2>Home</h2>
         <p>Welcome to the Comic Book Library!</p>
+        {/* <img src="/psylocke.jpg" alt="Psylocke" className="psylocke-image" /> */}
     </div>
     );
 };
