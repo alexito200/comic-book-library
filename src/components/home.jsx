6e09c3d345478b52d64557262ee3e11003948710
alex <alexalarcon200@gmail.com>
2025-02-11 14:05:25 -0500

    changed the home title

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
     <div className="home">
-        <h2>Home</h2>
+        <h2>Marvel API</h2>
         <p>Welcome to the Comic Book Library!</p>
         {/* <img src="/psylocke.jpg" alt="Psylocke" className="psylocke-image" /> */}
     </div>
