8785ed4f6b6bda80c902b4db7e2a3dc033310451
alexito200 <alexalarcon200@gmail.com>
2025-02-26 00:22:19 -0500

    added responsiveness to all pages

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,6 @@ const Home = () => {
     return (
     <div className="home">
         <h2 className="homeTitle">Marvel <span>API</span></h2>
-        {/* <p className="homeWelcome">Welcome to the Comic Book Library!</p> */}
     </div>
     );
 };
